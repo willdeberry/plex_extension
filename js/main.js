d87749a799cc88dc17f72357489faa1ffbde5254
Will DeBerry <willdeberry@gmail.com>
2015-09-09 00:44:37 -0400

    Add theming

--- js/main.js
+++ js/main.js
@@ -6,13 +6,11 @@ function sectionListings(key) {
 
 function generateUI(data) {
 	$.each(data, function(index, section) {
-		var sectionName = section.name;
-		var sectionKey = section.key;
-		var sectionItems = section.items;
-		$('#main').append('<div id="' + sectionKey + '" onClick="sectionListings(' + sectionKey + ')">' + sectionName + '</div>');
-		$.each(sectionItems, function(index, item) {
-			console.log(sectionKey);
-			$('#' + sectionKey).append('<div id="' + item.id + '">' + item.title + '</div>');
+		$('#main').append('<div class="panel panel-default" id="panel' + section.key + '">');
+		$('#panel' + section.key).append('<div class="panel-heading" id="panel-head' + section.key + '" onClick="sectionListings(' + section.key + ')">' + section.name + '</div>');
+		$('#panel' + section.key).append('<div class="list-group" id="list-group' + section.key + '"></div>');
+		$.each(section.items, function(index, item) {
+			$('#list-group' + section.key).append('<div class="list-group-item" id="' + item.id + '">' + item.title + '</div>');
 		});
 	});
 }
