7f101fb56254982ec7c039634aa1e10195ea61fc
Will DeBerry <willdeberry@gmail.com>
2015-09-09 23:52:47 -0400

    Continuing to build up the UI

--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
 var fullListings = [];
 
-function sectionListings(key) {
-	console.log('here');
+function toggleCollapse(key) {
+	$('.in').collapse('toggle');
+	$('#list-group' + key).collapse('toggle');
 }
 
 function generateUI(data) {
 	$.each(data, function(index, section) {
-		$('#main').append('<div class="panel panel-default" id="panel' + section.key + '">');
-		$('#panel' + section.key).append('<div class="panel-heading" id="panel-head' + section.key + '" onClick="sectionListings(' + section.key + ')">' + section.name + '</div>');
-		$('#panel' + section.key).append('<div class="list-group" id="list-group' + section.key + '"></div>');
+		$('#main').prepend('<div class="panel panel-default" id="panel' + section.key + '">');
+		$('#panel' + section.key).append(
+			'<div class="panel-heading" id="panel-head' + section.key + '" role="tab">'
+				+ '<h4 class="panel-title">' + section.name + '</h4>'
+			+ '</div>'
+		);
+		$('#panel' + section.key).append('<div class="list-group panel-collapse collapse" id="list-group' + section.key + '" role="tabpanel" aria-labelledby="panel-head' + section.key + '" aria-expanded="false"></div>');
 		$.each(section.items, function(index, item) {
-			$('#list-group' + section.key).append('<div class="list-group-item" id="' + item.id + '">' + item.title + '</div>');
+			$('#list-group' + section.key).append(
+				'<div class="list-group-item" id="' + item.id + '">'
+					+ '<div class="col-xs-6 col-sm-6">'
+						+ '<img class="img-thumbnail" src="' + server + item.thumb + '"/>'
+					+ '</div>'
+					+ '<div class="col-xs-6 col-sm-6">'
+						+ item.title
+					+ '</div>'
+					+ '<div class="col-xs-6 col-sm-6">'
+						+ '<button class="btn btn-default" type="submit">Button</button>'
+					+ '</div>'
+				+ '</div>'
+			);
 		});
+		$('#panel-head' + section.key).on('click', function() { toggleCollapse(section.key); });
 	});
 }
 
