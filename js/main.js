e6288060e99f4be76512b33497e23fc2644a68fd
Will DeBerry <willdeberry@gmail.com>
2015-10-06 22:57:49 -0400

    Better error catching with localStorage

--- js/main.js
+++ js/main.js
@@ -4,33 +4,50 @@ function toggleCollapse(key) {
 }
 
 function main(authToken) {
+	function fail() {
+		gatherData(authToken).then(function(data) {
+			chrome.storage.local.set({data: data}, function() { console.log('Data saved off') });
+			generateUI(data);
+		});
+	}
+
 	chrome.storage.local.get('data', function(result) {
 		if (chrome.runtime.lastError) {
-			gatherData(authToken).then(function(data) {
-				chrome.storage.local.set({data: data}, function() { console.log('Data saved off') });
-				generateUI(data);
-			});
-		} else {
+			console.log('1');
+			fail();
+		} else if (result.data) {
+			console.log('2');
 			generateUI(authToken, result.data);
+		} else {
+			console.log('3');
+			fail();
 		}
 	});
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+	function go(authToken) {
+		$('#login').addClass('hidden');
+		$('#content').removeClass('hidden');
+		main(authToken);
+	}
+
+	function fail() {
+		$('#loginButton').on('click', function(event) {
+			event.preventDefault();
+			var authToken = onLogin();
+			go(authToken);
+		});
+	}
+
 	chrome.storage.local.get('authToken', function(result) {
-		if(chrome.runtime.lastError) {
-			$('#loginButton').on('click', function(event) {
-				event.preventDefault();
-				var authToken = onLogin();
-				$('#login').addClass('hidden');
-				$('#content').removeClass('hidden');
-				main(authToken);
-			});
-		} else {
+		if (chrome.runtime.lastError) {
+			fail();
+		} else if (result.authToken) {
 			var authToken = result.authToken;
-			$('#login').addClass('hidden');
-			$('#content').removeClass('hidden');
-			main(authToken);
+			go(authToken);
+		} else {
+			fail();
 		}
 	});
 });
